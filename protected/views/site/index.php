d1f04be5eda57b8b5e18791ecb4e5bed52f2b35a
root <diego.venezuela@gmail.com>
2016-08-18 10:41:11 +0200

    A back counter has been added

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -14,15 +14,34 @@ $this->pageTitle=Yii::app()->name;
 ?>
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<button id="starter">Start</button>
+<div id="counter"></div>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
+<?php
+yii::app()->clientScript->registerScript("counter", "
+$(\"#starter\").click(function(){
+    console.log('The counter has started');
+    var timeLeft = 1500;
+    function zeroAdder(value){
+        if (value < 10 ){
+            return '0'+value;
+        }
+        return value;
+    }
+    function secondsToTime( seconds ){
+        if (seconds < 60 ){
+            return '00:'+ seconds;
+        } else {
+            return  zeroAdder(Math.floor(seconds/60))+':'+zeroAdder(seconds % 60); 
+        }
+    }
+    setInterval(function( timeleft ){
+        console.log('ha entrado en el interval');
+        timeLeft--;
+        $(\"#counter\").html( secondsToTime(timeLeft) ); 
+    }, 1000);
+    
+});
+");
 
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+?>
